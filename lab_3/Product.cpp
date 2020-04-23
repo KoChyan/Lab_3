c553bd7c836d8490a3c5f64376df689307c0bb63
KoChyan <64205325+KoChyan@users.noreply.github.com>
2020-04-23 20:35:05 +0400

    Update Product.cpp

--- lab_3/Product.cpp
+++ lab_3/Product.cpp
@@ -6,14 +6,14 @@
 void Product::setName(string name) {
     this->name = name;
 }
-string Product::getName() {return name;}
+string Product::getName() const {return name;}
 
 void Product::setColor(string color) {
     this->color = color;
 }
-string Product::getColor() {return color;}
+string Product::getColor() const {return color;}
 
 void Product::setSize(int size){
     this->size = size;
 }
-int Product::getSize(){return size;}
+int Product::getSize() const {return size;}
